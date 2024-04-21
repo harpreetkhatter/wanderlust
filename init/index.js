96a2243891c5aea9a8acdca45bd8eb08f157fe3f
harpreet <khatterharpreet@gmail.com>
2024-04-21 12:41:00 +0530

    Add Project Files

---
+++ init/index.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+const initData =require("./data.js");
+const Listing = require("../models/listing.js");
+
+
+main().then(()=>{
+    console.log("connected to db")
+})
+.catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/wanderlust');
+
+  
+}
+
+const initDb = async ()=>{
+   await Listing.deleteMany({});
+    initData.data = initData.data.map((obj)=>({ ...obj,owner:'661b5415307da0eb020fd1a8' }))
+   await Listing.insertMany(initData.data);
+   console.log("data was initialised");
+};
+
+initDb();
---
+++ routes/review.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const router = express.Router({mergeParams: true});
+const wrapAsync = require("../utils/wrapAsync.js");
+const Review = require("../models/reviews.js");
+const Listing= require ("../models/listing.js");
+const {validateReview,isLoggedIn,isReviewAuthor} = require("../middleware.js");
+
+const reviewController = require("../controllers/reviews.js");
+
+//REVIEWS
+//POST REVIEW ROUTE
+router.post("/",isLoggedIn,validateReview,wrapAsync(reviewController.createReview));
+//DELETE REVIEW ROUTE
+router.delete("/:reviewId",isLoggedIn,isReviewAuthor,wrapAsync(reviewController.destroyReview));
+
+
+
+
+module.exports = router;
